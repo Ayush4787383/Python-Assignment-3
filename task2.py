45b822ef921497dbb339eae6704c60b738b94586
Ayush Yadav <ayu362336@gmail.com>
2026-02-01 21:32:26 +0530

    Add files via upload

---
+++ task2.py
@@ -0,0 +1,8 @@
+import math
+num = int(input("Enter a number: "))
+sq_root = math.sqrt(num)
+log_value = math.log(num)
+sine_value = math.sin(num)
+print("Square Root:", sq_root)
+print("Logarithm:", log_value)
+print("Sine Value:", sine_value)
